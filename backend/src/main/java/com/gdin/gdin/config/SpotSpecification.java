cb0be97f18aa671c91a7be09194e69c238d06dc6
Stefan Matic <stefanmatic941@gmail.com>
2024-05-11 13:35:16 +0200

    Implementing searching

--- backend/src/main/java/com/gdin/gdin/config/SpotSpecification.java
+++ backend/src/main/java/com/gdin/gdin/config/SpotSpecification.java
@@ -49,36 +49,47 @@ public class SpotSpecification {
             if (workingTo != null) {
                 predicate = builder.and(predicate, builder.equal(root.get("workingTo"), workingTo));
             }
+
             if (alwaysOpen != null && alwaysOpen) {
                 predicate = builder.and(predicate, builder.equal(root.get("alwaysOpen"), alwaysOpen));
             }
+
             if (outdoorSeating != null && outdoorSeating) {
                 predicate = builder.and(predicate, builder.equal(root.get("outdoorSeating"), outdoorSeating));
             }
+
             if (wifiAvailable != null && wifiAvailable) {
                 predicate = builder.and(predicate, builder.equal(root.get("wifiAvailable"), wifiAvailable));
             }
+
             if (parking != null && parking) {
                 predicate = builder.and(predicate, builder.equal(root.get("parking"), parking));
             }
+
             if (petsAllowed != null && petsAllowed) {
                 predicate = builder.and(predicate, builder.equal(root.get("petsAllowed"), petsAllowed));
             }
+
             if (hasSpecialDietaryOptionVegetarian != null && hasSpecialDietaryOptionVegetarian) {
                 predicate = builder.and(predicate, builder.equal(root.get("hasSpecialDietaryOptionVegetarian"), hasSpecialDietaryOptionVegetarian));
             }
+
             if (hasSpecialDietaryOptionVegan != null && hasSpecialDietaryOptionVegan) {
                 predicate = builder.and(predicate, builder.equal(root.get("hasSpecialDietaryOptionVegan"), hasSpecialDietaryOptionVegan));
             }
+
             if (hasSpecialDietaryOptionGlutenFree != null && hasSpecialDietaryOptionGlutenFree) {
                 predicate = builder.and(predicate, builder.equal(root.get("hasSpecialDietaryOptionGlutenFree"), hasSpecialDietaryOptionGlutenFree));
             }
+
             if (hasFitnessMenu != null && hasFitnessMenu) {
                 predicate = builder.and(predicate, builder.equal(root.get("hasFitnessMenu"), hasFitnessMenu));
             }
+
             if (hasPosnaFood != null && hasPosnaFood) {
                 predicate = builder.and(predicate, builder.equal(root.get("hasPosnaFood"), hasPosnaFood));
             }
+
             if (hasBreakfast != null && hasBreakfast) {
                 predicate = builder.and(predicate, builder.equal(root.get("hasBreakfast"), hasBreakfast));
             }
@@ -86,18 +97,22 @@ public class SpotSpecification {
             if (spotType != null) {
                 predicate = builder.and(predicate, builder.equal(root.get("spotType"), spotType));
             }
+
             if (musicTypes != null && !musicTypes.isEmpty()) {
                 Join<Spot, MusicTypes> musicJoin = root.join("musicTypes");
                 predicate = builder.and(predicate, musicJoin.in(musicTypes));
             }
+
             if (ambianceTypes != null && !ambianceTypes.isEmpty()) {
                 Join<Spot, AmbianceTypes> ambianceJoin = root.join("ambianceTypes");
                 predicate = builder.and(predicate, ambianceJoin.in(ambianceTypes));
             }
+
             if (cuisineTypes != null && !cuisineTypes.isEmpty()) {
                 Join<Spot, CuisineTypes> cuisineJoin = root.join("cuisineTypes");
                 predicate = builder.and(predicate, cuisineJoin.in(cuisineTypes));
             }
+
             if (availableActivities != null && !availableActivities.isEmpty()) {
                 Join<Spot, AvailableActivities> activitiesJoin = root.join("availableActivities");
                 predicate = builder.and(predicate, activitiesJoin.in(availableActivities));
